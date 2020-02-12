223722af8fbdd32ddd7ab075e39cdf751147ff52
Santiago Bedoya <santiago.b9826@gmail.com>
2020-02-12 15:12:10 -0500

    Add tsp homework

---
+++ tsp/launch.sh
@@ -0,0 +1,7 @@
+file_name="tsp-bf"
+clear
+rm $file_name.out
+gcc -Wall -fopenmp -o $file_name.out $file_name.c
+[ $2 ] && a=$2  || a="default"
+printf "Size: %s and Seed: %s\n\n" $1 $a
+./$file_name.out $1 $2
